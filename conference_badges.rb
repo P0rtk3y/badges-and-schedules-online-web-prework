7ad7c55a300543ea25ceedf55baf48197d5678cf
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 08:43:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(name_array)
   room_assign = []
-  name_array.each_with_index{|name, room| room_assign << "Hello, #{name}! You'll be assigned to room #{room}!"}
+  name_array.each_with_index{|name, room| room_assign.push("Hello, #{name}! You'll be assigned to room #{room}!")}
   end
   return room_assign
 end
