df31717d70b133d7f1afdcedc9ea343ff0840f41
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 08:41:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,17 +3,4 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name_array)
-  new_name_array = []
-  name_array.each do |name|
-    new_name_array << badge_maker(name)
-  end
-  return new_name_array
-end
 
-def assign_rooms(name_array)
-  room_assign = []
-  name_array.each_with_index{|name, room| room_assign << "Hello, #{name}! You'll be assigned to room #{room}!"}
-  end
-  return room_assign
-end
